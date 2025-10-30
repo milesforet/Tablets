a5a887a4981f0eba119e63e5d69c191b1bf76442
milesforet <milesforet@hotmail.com>
2025-10-30 14:38:14 -0400

    update to reboot all tablets

--- RebootTablets.ps1
+++ RebootTablets.ps1
@@ -1,123 +1,89 @@
-Import-Module Microsoft.Graph.Identity.DirectoryManagement
-Import-Module Microsoft.Graph.DeviceManagement
-#TODO: WILL NEED SERVICE ACCOUNT OR APP WITH PERMISSIONS
+[System.Collections.ArrayList]$errors= @()
 
 try{
-    Connect-MgGraph -Scopes "DeviceManagementManagedDevices.PrivilegedOperations.All", "Group.Read.All" -NoWelcome
-    Write-Output "Graph auth successful...`n"
-}catch{
-    Write-Output "ERROR connecting to graph- $_"
-    exit
-}
-
-
-<#------------------------------------------------------------------- 
-                    REBOOT ALL ANDROID TABLETS
---------------------------------------------------------------------#>
-
-<# #get android devices/filter out non-androidEnterpriseDedicatedDevice (this removes a few manually enrolled Androids)
-$androidDevices = Get-MgDeviceManagementManagedDevice -All -Filter "OperatingSystem eq 'Android'"
-$androidDevices = $androidDevices | Where-Object DeviceEnrollmentType -eq "androidEnterpriseDedicatedDevice"
-
-#loop through devices and send reboot
-foreach($tablet in $androidDevices){
     try{
-        Restart-MgDeviceManagementManagedDeviceNow -ManagedDeviceId $tablet.Id
-        Write-Host "$($tablet.SerialNumber) - reboot initiated"
+        Connect-MgGraph -Scopes "DeviceManagementManagedDevices.PrivilegedOperations.All", "Group.Read.All" -NoWelcome
+        Write-Output "Graph auth successful...`n"
     }catch{
-        Write-Host "Failed to reboot ${$tablet.SerialNumber} - $_"
+        throw "ERROR connecting to graph- $_"
     }
-} #>
 
+    $url = "https://graph.microsoft.com/v1.0/deviceManagement/managedDevices?`$filter=OperatingSystem eq 'Android'"
 
-<#------------------------------------------------------------------- 
-                    REBOOT ALL DEVICES IN A GROUP
---------------------------------------------------------------------#>
+    [System.Collections.ArrayList]$tablets = @()
+    $morePages = $true
+    
+    while($morePages){
+        $androidDevices = Invoke-MgGraphRequest -Method GET -Uri $url
+        
+        if(!$androidDevices."@odata.nextLink"){
+            
+            $morePages = $false
+        }else{
+            $url = $androidDevices."@odata.nextLink"
+        }
+    
 
-#get group info
-$groupName = "TESTDEVICES_sg"
-#$groupName = "sg-app-Pilot-SCD"
-#$groupName = "sg-SCD-WEST"
-$group = (Invoke-MgGraphRequest -Method GET -Uri "https://graph.microsoft.com/v1.0/groups?`$search=`"displayName:$groupName`"" -Headers @{ConsistencyLevel = "eventual"}).Value
+        $tablets += $androidDevices.value | Where-Object {$_."deviceEnrollmentType" -eq "androidEnterpriseDedicatedDevice"}
 
-#get members of group using group id
-[System.Collections.ArrayList]$tabletsInGroup = @()
-$url = "https://graph.microsoft.com/v1.0/groups/$($group.Id)/members"
-$morePages = $true
-
-while ($morePages) {
-    $members = Invoke-MgGraphRequest -Method GET -Uri $url
-    $tabletsInGroup += $members.value
-
-    if($members."@odata.nextLink"){
-        $url = $members."@odata.nextLink"
-        continue
     }
-    $morePages = $false
+
+    #loop through devices and send reboot
+    foreach($tablet in $tablets){
+        try{
+            Invoke-GraphRequest -Method POST -Uri "https://graph.microsoft.com/v1.0/deviceManagement/managedDevices/$($tablet.Id)/rebootNow"
+            #Restart-MgDeviceManagementManagedDeviceNow -ManagedDeviceId $tablet.Id
+            Write-Output "$($tablet.SerialNumber) - reboot initiated"
+        }catch{
+            Write-Output "Failed to reboot ${$tablet.SerialNumber} - $_"
+            $errors.Add("Failed to reboot ${$tablet.SerialNumber} - $_") | Out-Null
+        }
+    } 
+}catch{
+    $errors.Add($_) | Out-Null
 }
 
+$errors
 
-foreach($tablet in $tabletsInGroup){
-    #get Intune ID from AzureADDeviceID (this is needed for the reboot)
-    $tabletIntuneId = (Invoke-GraphRequest -Method GET -Uri "https://graph.microsoft.com/v1.0/deviceManagement/managedDevices?`$filter=azureADDeviceId+eq+'$($tablet.deviceId)'&`$select=id,serialNumber").Value
+exit
 
-    if(-not $tabletIntuneId){
-        Write-Host "$($tablet.displayName) - device not found... continuing to next device" -ForegroundColor Red
-        continue
+try{
+    #SEND EMAIL WITH ANY ERRORS
+    if(!$errors){
+        "Completed with no errors!!"
+        exit
+    }
+
+    $smtpCreds = Get-AbsCred -credName "smtp2go"
+
+    $errorEmailBody = ""
+
+    foreach($err in $errors){
+        $errorEmailBody += "<p>$err</p><br>"   
+    }
+
+    $emailHeaders = @{
+        "Content-Type" = "application/json"
+        "X-Smtp2go-Api-Key" = $smtpCreds.GetNetworkCredential().Password
+        "Accept" = "application/json"
     }
     
-    #reboot tablet
-    #Invoke-GraphRequest -Method POST -Uri "https://graph.microsoft.com/v1.0/deviceManagement/managedDevices/$($tabletIntuneId.id)/rebootNow"
-    Write-Output "$($tabletIntuneId.serialNumber) - reboot initiated"
-}
+    $emailBody = @{
+        "sender" = "mforet@abskids.com"
+        "to" = @("mforet@abskids.com")
+        "Subject" = "Error(s) in Weekly Tablet Reboot"
+        "html_body" = $errorEmailBody
+    } | ConvertTo-Json
 
-
-
-#loop through members of group
-<# foreach($tablet in $members){
-
-    try{
-        #get Intune ID from AzureADDeviceID (this is needed for the reboot)
-        $tabletIntuneId = (Invoke-GraphRequest -Method GET -Uri "https://graph.microsoft.com/v1.0/deviceManagement/managedDevices?`$filter=azureADDeviceId+eq+'$($tablet.deviceId)'&`$select=id,serialNumber").Value
-
-        if(-not $tabletIntuneId){
-            Write-Host "$($tablet.displayName) - device not found... continuing to next device" -ForegroundColor Red
-            continue
-        }
-        
-        #reboot tablet
-        #Restart-MgDeviceManagementManagedDeviceNow -ManagedDeviceId $tabletIntuneId.id
-        Write-Host "$($tabletIntuneId.serialNumber) - reboot initiated" -ForegroundColor Green
-        $count++
-    }catch{
-        Write-Host "ERROR: - $_" -ForegroundColor Red
-    }
-} #>
-
-
-
-
-#testing paging through to call endpoint with Invoke-MgGraphRequest
-
-<# $url = "https://graph.microsoft.com/v1.0/deviceManagement/managedDevices?`$filter=OperatingSystem+eq+'Android'"
-$tablets
-$isMoreTablets = $true
-while($isMoreTablets){
-
-    Write-Host $count
-    $response = Invoke-MgGraphRequest -Method GET -uri $url
-    $tablets += $response.value
-
-    if($response."@odata.nextLink"){
-        $url = $response."@odata.nextLink"
-        continue
+    $sendEmailRes = Invoke-WebRequest -Method "POST" -Uri "https://api.smtp2go.com/v3/email/send" -Headers $emailHeaders -Body $emailBody -SkipHttpErrorCheck
+    
+    if($sendEmailRes.StatusCode -ne 200){
+        throw "Failed to send errror email"
     }
 
-    $isMoreTablets = $false
-}
-Write-Host "Test" -ForegroundColor Green
-$tablets.Count
-$tablets = $tablets | Where-Object DeviceEnrollmentType -eq "androidEnterpriseDedicatedDevice"
-$tablets.Count #>
+    Write-Output "Email with errors sent!"
 
-$accessToken = ConvertTo-SecureString 
+}catch{
+    $_
+    exit
+}
